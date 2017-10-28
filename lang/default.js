d0ac26fd723df297b0426bf413889b1205fb780a
Sergii Lischuk <evertodante@gmail.com>
2017-10-28 15:51:39 +0300

    Removed bool initialization (#4)
    * Removed boolean initialization

---
+++ lang/default.js
@@ -0,0 +1,8 @@
+const lang = {
+  repoName: 'Repository name: ',
+  repoDesciption: 'Description: ',
+  repoReadme: 'Initialize Readme? ',
+  repoMakePrivate: 'Make Repository private?'
+};
+
+module.exports = lang;
